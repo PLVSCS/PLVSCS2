ae8e90e264bb3b7ac05c99bc71e934902dae02af
almarglenmarcelo <105777332+PLVSCS@users.noreply.github.com>
2022-05-20 22:24:49 +0800

    scannedimg

--- routes/event.js
+++ routes/event.js
@@ -16,7 +16,7 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/uploads/')
+    cb(null, '../public/uploads/')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
