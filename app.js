fc8f6fb27339233804b1d738a308ae75aa87f654
Joseph Magat <52060595+xephyre619@users.noreply.github.com>
2022-05-20 14:24:42 +0800

    fx

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ var connection = mysql.createConnection({
   user     :'plvscs',
   password : 'plvscsdb2022',
   database:'plvscdb',
-  multipleStatements: true
-  
+  multipleStatements: true  
 });
 
 
@@ -44,18 +43,18 @@ connection.connect((err)=>{
 
 
 function handleDisconnect() {
-  var con = mysql.createConnection(connection); // Recreate the connection, since
+  //var con = mysql.createConnection(connection); // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  con.connect(function(err) {              // The server is either down
+  connection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       //setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  con.on('error', function(err) {
-    con.log('db error', err);
+  connection.on('error', function(err) {
+    connection.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       //handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
