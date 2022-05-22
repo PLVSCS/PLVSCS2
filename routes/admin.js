f61c1f02c5037604c4ff5b5e535df2c5c8bd9ae1
Joseph Magat <52060595+xephyre619@users.noreply.github.com>
2022-05-22 21:53:24 +0800

    fx

--- routes/admin.js
+++ routes/admin.js
@@ -543,13 +543,13 @@ router.post('/create/:entity',upload.single('scannedimage'),async(req,res)=>{
   else if (req.params.entity ==="oic") {
 
     //mysql
-    sqldb.query(`insert into admin(idNo,email,surname,contactNo,firstName,middleName,departmentCode,department,password,adminType) values('${req.body.idnumber}',
+    sqldb.query(`insert into admin(idNo,email,surname,contactNo,firstName,middleName,departmentCode,department,password,adminType) values('${parseInt(req.body.idnumber)}',
       '${req.body.email}',
       '${req.body.Surname}',
       '${req.body.contactnumber}',
       '${req.body.firstname}',
       '${req.body.middlename}',
-      '${req.body.departmentcode}',
+      '${parseInt(req.body.departmentcode)}',
       '${req.body.department}',
       '${req.body.password}',
       'oic'
