dd2b1e93254c2a9f96e9ac567b516eb75cf9272d
almarglenmarcelo <105777332+PLVSCS@users.noreply.github.com>
2022-05-26 16:46:36 +0800

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ var aes256 = require('aes256');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads/')
+    cb(null, '..public/uploads/')
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname)
