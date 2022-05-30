cd7512b2548f6aa31cba8e75a9253f39c370f0bb
almarglenmarcelo <105777332+PLVSCS@users.noreply.github.com>
2022-05-30 21:11:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
   host     :'localhost',
-  user     :'root',
-password :'', 
-database :'plvscdb',
+  user     :'plvscs',
+  password :'plvscsdb2022',
+  database :'plvscdb',
 // user     :'root',
 // password :'',
 // database :'jrelozdb',
